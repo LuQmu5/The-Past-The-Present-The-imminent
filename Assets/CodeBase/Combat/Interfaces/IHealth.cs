e3a17c6e7adeccb588da91123656702846f0b9fe
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-11-09 17:00:14 +0400

    created

---
+++ Assets/CodeBase/Combat/Interfaces/IHealth.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public interface IHealth
+{
+    public float MaxHealth { get; set; }
+    public float CurrentHealth { get; set; }
+
+    public void ApplyDamage(float amount);
+}
---
+++ Assets/CodeBase/Combat/MeleeWeapon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour, IMeleeWeapon
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out IHealth hittedTarget))
+        {
+            DealDamage(hittedTarget);
+        }
+    }
+
+    public void DealDamage(IHealth target)
+    {
+        target.ApplyDamage(1);
+        print(target.CurrentHealth);
+    }
+}
